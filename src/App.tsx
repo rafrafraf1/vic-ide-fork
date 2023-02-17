1d0c51e362bad2e47e6f0d463974ce48f429b5fd
atanf <124199629+atanf@users.noreply.github.com>
2023-02-17 07:53:01 +0000

    Flesh out UI for Memory and CPU

--- src/App.tsx
+++ src/App.tsx
@@ -2,156 +2,15 @@ import "./App.css"; // eslint-disable-line @typescript-eslint/no-import-type-sid
 import * as React from "react";
 import { type ComputerState, newComputerState } from "./Computer/Computer";
 import { Computer } from "./UI/Computer";
-import { ValueCellInput } from "./UI/ValueCellInput";
 
 function App(): JSX.Element {
   const computer = React.useRef<ComputerState>(newComputerState());
 
   return (
-    <div className="App">
-      <div style={{ margin: "20px" }}>
-        <ValueCellInput value={323} />
-      </div>
-      <Computer computer={computer.current} />
-      <main className="container">
-        <div className="Vic-Simulator">
-          <div>
-            <InputIo />
-            <OutputIo />
-          </div>
-          <Cpu />
-          <MemorySegment />
-        </div>
-      </main>
+    <div className="App-Root">
+      <div className="App-Toolbar-Cont">VIC Toolbar</div>
+      <Computer className="App-Computer-Cont" computer={computer.current} />
     </div>
-  );
-}
-
-function Cpu(): JSX.Element {
-  return (
-    <div>
-      <CpuRegister name="Instruction Register" value={0} />
-      <CpuRegister name="Data Register" value={0} />
-      <CpuRegister name="Program Counter" value={0} />
-    </div>
-  );
-}
-
-interface CpuRegisterProps {
-  name: string;
-  value: number;
-}
-
-function CpuRegister(props: CpuRegisterProps): JSX.Element {
-  return (
-    <article>
-      <div>{props.name}</div>
-      <input value={props.value} />
-    </article>
-  );
-}
-
-function InputIo(): JSX.Element {
-  return (
-    <article className="Vic-Input">
-      <h6>Input</h6>
-      <input className="Vic-Input-Active" value="" />
-      <input value="" />
-      <input value="" />
-      <input value="" />
-      <input value="" />
-      <input value="" />
-    </article>
-  );
-}
-
-function OutputIo(): JSX.Element {
-  return (
-    <article className="Vic-Input">
-      <h6>Output</h6>
-      <input className="Vic-Output-Active" value="" />
-      <input value="" />
-      <input value="" />
-      <input value="" />
-      <input value="" />
-      <input value="" />
-    </article>
-  );
-}
-
-function MemorySegment(): JSX.Element {
-  return (
-    <article>
-      <h6>Main Memory</h6>
-      <div className="Vic-Memory-Cell">
-        <label>00</label>
-        <input value="800" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>01</label>
-        <input value="900" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>02</label>
-        <input value="500" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>03</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>04</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>05</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>06</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>07</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>08</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>09</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>10</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>11</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>12</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>13</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>14</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>15</label>
-        <input value="0" />
-      </div>
-      <div className="Vic-Memory-Cell">
-        <label>16</label>
-        <input value="0" />
-      </div>
-    </article>
   );
 }
 
--- src/UI/Computer.tsx
+++ src/UI/Computer.tsx
@@ -5,27 +5,43 @@ import {
   memoryRead,
 } from "../Computer/Computer";
 import type { Address } from "../Computer/Instruction";
+import type { Value } from "../Computer/Value";
 import { ValueCellInput } from "./ValueCellInput";
+import classNames from "classnames";
 
 export interface ComputerProps {
+  className?: string;
   computer: ComputerState;
 }
 
 export function Computer(props: ComputerProps): JSX.Element {
   return (
-    <div>
-      <MainMemory memory={props.computer.memory} />
+    <div className={classNames(props.className, "Computer-Root")}>
+      <div className="Computer-Io">IO</div>
+      <div className="Computer-Divider Computer-Divider1"></div>
+      <Cpu
+        className="Computer-Cpu"
+        instructionRegister={0}
+        dataRegister={0}
+        programCounter={0}
+      />
+      <div className="Computer-Divider Computer-Divider2"></div>
+      <MainMemory className="Computer-Memory" memory={props.computer.memory} />
+      <div className="Computer-Io-Label Computer-Label">I/O Units</div>
+      <div className="Computer-Cpu-Label Computer-Label">CPU</div>
+      <div className="Computer-Memory-Label Computer-Label">Memory</div>
     </div>
   );
 }
 
 export interface MainMemoryProps {
+  className?: string;
   memory: MemoryCell[];
 }
 
 export function MainMemory(props: MainMemoryProps): JSX.Element {
   return (
-    <div className="Computer-MainMemory">
+    <div className={classNames(props.className, "Computer-MainMemory")}>
       <MemorySegment memory={props.memory} segmentStart={0} segmentEnd={49} />
       <MemorySegment memory={props.memory} segmentStart={50} segmentEnd={99} />
     </div>
@@ -57,4 +73,41 @@ function addressRange(low: Address, high: Address): Address[] {
     result.push(i);
   }
   return result;
+}
+
+interface CpuProps {
+  className?: string;
+  instructionRegister: Value;
+  dataRegister: Value;
+  programCounter: Value;
+}
+
+export function Cpu(props: CpuProps): JSX.Element {
+  return (
+    <div className={classNames(props.className, "Computer-Cpu")}>
+      <CpuRegister label="Instruction Register">
+        <ValueCellInput value={props.instructionRegister} />
+      </CpuRegister>
+      <CpuRegister label="Data Register">
+        <ValueCellInput value={props.dataRegister} />
+      </CpuRegister>
+      <CpuRegister label="Program Counter">
+        <ValueCellInput value={props.programCounter} />
+      </CpuRegister>
+    </div>
+  );
+}
+
+interface CpuRegisterProps {
+  children?: React.ReactNode;
+  label: string;
+}
+
+function CpuRegister(props: CpuRegisterProps): JSX.Element {
+  return (
+    <div className="Computer-CpuRegister-Root">
+      <header>{props.label}</header>
+      {props.children}
+    </div>
+  );
 }
