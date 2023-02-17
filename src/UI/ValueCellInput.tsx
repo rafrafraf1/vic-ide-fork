8a10248e201905e220b6befb4aa09087eaaa7157
atanf <124199629+atanf@users.noreply.github.com>
2023-02-17 09:51:36 +0000

    ValueCellInputProps change value implementation

--- src/UI/ValueCellInput.tsx
+++ src/UI/ValueCellInput.tsx
@@ -1,5 +1,6 @@
 import "./ValueCellInput.css"; // eslint-disable-line @typescript-eslint/no-import-type-side-effects
-import { type ChangeEvent, type FormEvent, createRef, useState } from "react";
+import * as React from "react";
+import type { ChangeEvent, FormEvent } from "react";
 import type { Value } from "../Computer/Value";
 
 export interface ValueCellInputProps {
@@ -12,31 +13,73 @@ export interface ValueCellInputProps {
 }
 
 export function ValueCellInput(props: ValueCellInputProps): JSX.Element {
-  const [inputStr, setInputStr] = useState<string>(`${props.value}`);
+  const { value, onValueChange } = props;
+
+  const [inputStr, setInputStr] = React.useState<string>(`${value}`);
+
+  const inputRef = React.createRef<HTMLInputElement>();
+
+  // React to changes to the "value" prop
+  React.useEffect(() => {
+    // If we are focused then we ignore the change. The user is editing the
+    // value and that is what takes preference.
+    if (document.activeElement === inputRef.current) {
+      return;
+    }
+
+    if (value !== parseInt(inputStr, 10)) {
+      setInputStr(`${value}`);
+    }
+  }, [inputRef, inputStr, value]);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
     setInputStr(sanitizeValue(e.target.value));
   };
 
   const handleFocus = (): void => {
+    // When the user focuses the input element, we want to to select the input
+    // text, so that it is ready to be replaced.
     if (inputRef.current !== null) {
       inputRef.current.select();
     }
   };
 
-  const handleBlur = (): void => {
-    console.log("BLUR");
-  };
+  // Called after the user has confirmed the entered value
+  const updateValue = React.useCallback((): void => {
+    const value = inputStr === "" ? 0 : parseInt(inputStr, 10);
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    console.log("SUBMIT");
-    if (inputRef.current !== null) {
-      inputRef.current.blur();
+    // If the "inputStr" has any leading zeroes then we remove them:
+    if (inputStr !== `${value}`) {
+      setInputStr(`${value}`);
     }
-  };
 
-  const inputRef = createRef<HTMLInputElement>();
+    if (onValueChange !== undefined) {
+      onValueChange(value);
+    }
+  }, [inputStr, onValueChange]);
+
+  // This happens when the user tabs out of the input element, or clicks
+  // outside of it.
+  const handleBlur = React.useCallback((): void => {
+    console.log("BLUR");
+    updateValue();
+  }, [updateValue]);
+
+  // This happens when the user presses enter when the input element is
+  // focused.
+  const handleSubmit = React.useCallback(
+    (e: FormEvent<HTMLFormElement>): void => {
+      // Prevent the default behaviour of a page refresh:
+      e.preventDefault();
+
+      console.log("SUBMIT");
+      if (inputRef.current !== null) {
+        // TODO Select next input (according to tab order) instead of "blur"
+        inputRef.current.blur();
+      }
+    },
+    [inputRef]
+  );
 
   return (
     <form className="ValueCellInput" onSubmit={handleSubmit}>
