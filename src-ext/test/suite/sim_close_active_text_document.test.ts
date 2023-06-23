77ce26fea1d10cbf230d59952c7d1a96b37143b7
atanf <124199629+atanf@users.noreply.github.com>
2023-06-23 13:48:46 +0000

    Change "sim_close_active_text_document.test" because it was flaky
    The reason it was flaky is unknown. It may be because it was closing the
    Simulator instead of the TextDocument,

--- src-ext/test/suite/sim_close_active_text_document.test.ts
+++ src-ext/test/suite/sim_close_active_text_document.test.ts
@@ -12,19 +12,19 @@ import { step } from "../infra/TestSteps";
 import { testCase } from "../infra/TestCase";
 
 export const run = testCase(async (): Promise<void> => {
-  const simulatorManager = await step("Open Simulator", async () => {
-    await vscode.commands.executeCommand(vicOpenSimulatorCommand);
-    const simulatorManager = getSimulatorManager();
-    await waitForSimulatorReady(simulatorManager);
-    return simulatorManager;
-  });
-
   await step("Open Text Document", async () => {
     const textDocument = await vscode.workspace.openTextDocument({
       language: vicLanguageId,
       content: "// Test file",
     });
-    await vscode.window.showTextDocument(textDocument, vscode.ViewColumn.Two);
+    await vscode.window.showTextDocument(textDocument);
+  });
+
+  const simulatorManager = await step("Open Simulator", async () => {
+    await vscode.commands.executeCommand(vicOpenSimulatorCommand);
+    const simulatorManager = getSimulatorManager();
+    await waitForSimulatorReady(simulatorManager);
+    return simulatorManager;
   });
 
   const sourceFile1 = await step("Get Source File 1", async () => {
@@ -38,6 +38,11 @@ export const run = testCase(async (): Promise<void> => {
       id: "untitled:Untitled-1",
       hasErrors: false,
     },
+  });
+
+  await step("Switch back to Text Document", async () => {
+    await vscode.commands.executeCommand("workbench.action.previousEditor");
+    await waitForSimulatorReady(simulatorManager);
   });
 
   await step("Close current editor", async () => {
